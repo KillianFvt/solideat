5b87e4c6661659fc6117e028092c5a5678969d0a
KillianFvt <killian.fauvette@gmail.com>
2024-04-11 12:31:59 +0200

    first commit

---
+++ solid_eat/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for solid_eat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solid_eat.settings')
+
+application = get_asgi_application()
